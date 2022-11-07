d9b4f8d853e75728f2d56b025cff4464a0bacec2
lucilaarjona <lucilaarjona09@gmail.com>
2022-11-07 20:34:13 -0300

    change

--- api-music/src/main/java/com/spotify/music/ApiMusicApplication.java
+++ api-music/src/main/java/com/spotify/music/ApiMusicApplication.java
@@ -2,10 +2,12 @@ package com.spotify.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class ApiMusicApplication {
 
     public static void main(String[] args) {
--- api-playlist/src/main/java/com/spotify/playlist/ApiPlaylistApplication.java
+++ api-playlist/src/main/java/com/spotify/playlist/ApiPlaylistApplication.java
@@ -2,12 +2,14 @@ package com.spotify.playlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ApiPlaylistApplication {
 
     public static void main(String[] args) {
